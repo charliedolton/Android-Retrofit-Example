fe003aaca08b189420fbb8f12b375001efac8795
charliedolton <charliedolton1@gmail.com>
2022-02-08 12:57:25 -0800

    Most of the app completed... Still need to figure out Util class

---
+++ app/src/main/java/com/example/androidretrofitexample/apis/BookSearchService.java
@@ -0,0 +1,16 @@
+package com.example.androidretrofitexample.apis;
+
+import com.example.androidretrofitexample.VolumesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface BookSearchService {
+    @GET("/books/v1/volumes")
+    Call<VolumesResponse> searchVolumes(
+            @Query("q") String query,
+            @Query("inauthor") String author,
+            @Query("key") String apiKey
+    );
+}
